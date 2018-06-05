208957872e7af41849ec7242cc12ba775739a45b
Eric Guo <ericxguo@gmail.com>
2018-06-05 15:11:07 -0500

    added 3 test specs for our products api routes!

---
+++ server/api/tests/products.spec.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const { expect } = require('chai')
+const request = require('supertest')
+const app = require('../../index')
+const { Product, Category, ProductCategory } = require('../../db/models')
+
+describe('Product routes', () => {
+  describe('/api/products', () => {
+    const newProduct = {
+      name: 'Dog collar',
+      inventory: '5',
+      price: '16.50',
+      description: 'Stylish collar for only the coolest of pups'
+    }
+
+    const newCategory = {
+      name: 'dogs',
+      description: 'Canis lupus familiaris',
+      id: '1'
+    }
+
+    beforeEach(async () => {
+      await Product.create({...newProduct})
+    })
+
+    it('GET /api/products returns an array of products', () => {
+      return request(app)
+        .get('/api/products')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[0].name).to.be.equal(newProduct.name)
+        })
+    })
+
+    it('GET /api/products/:productId returns a single product', () => {
+      return request(app)
+        .get('/api/products/1')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('object')
+          expect(res.body.name).to.be.equal(newProduct.name)
+        })
+    })
+
+    it('GET /api/products/categories/:categoryId', async () => {
+      await Category.create({...newCategory})
+      await ProductCategory.create({ productId: '1', categoryId: '1' })
+      return request(app)
+        .get('/api/products/categories/1')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[0].name).to.be.equal(newProduct.name)
+        })
+    })
+  })
+})
