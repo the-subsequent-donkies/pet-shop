cca7b43b4c42e8193bf2834fefd2a45fd01893fb
Eric Guo <ericxguo@gmail.com>
2018-06-05 10:05:02 -0500

    created tests directory and test-setup.js

---
+++ tests/test-setup.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import db from '../server/db'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+beforeEach(() => {
+  return db.sync({force: true})
+})
