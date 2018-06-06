67147fe644e68fd4dd852c0ba8d13394978894b6
Eric Guo <ericxguo@gmail.com>
2018-06-05 22:08:22 -0500

    removed temp data

--- client/components/product-list.js
+++ client/components/product-list.js
@@ -5,41 +5,6 @@ import { connect } from 'react-redux'
 import IndividualProduct from './individual-product'
 import { getProductsServer } from '../store/product'
 
-const tempData = [
-  {
-    "id": "1",
-    "name": "Bone",
-    "inventory": "4",
-    "price": "2.50",
-    "imgUrl": "https://i.imgur.com/zzD13aO.jpg",
-    "description": "this is a fun chew toy for dogs"
-  },
-  {
-    "id": "2",
-    "name": "scratch post",
-    "inventory": "6",
-    "price": "22.50",
-    "imgUrl": "https://i.imgur.com/jI1kMrK.jpg",
-    "description": "Cats will scratch this"
-  },
-  {
-    "id": "3",
-    "name": "fake mouse",
-    "inventory": "9",
-    "price": "5.00",
-    "imgUrl": "https:/i.imgur.com/dQ7u2Wg.jpg",
-    "description": "Fake Mouse from the failing Meowyork times"
-  },
-  {
-    "id": "4",
-    "name": "Hamster wheel",
-    "inventory": "9",
-    "price": "7.50",
-    "imgUrl": "https://i.imgur.com/fFptBSA.jpg",
-    "description": "you spin my head right rond"
-  }
-]
-
 class ProductList extends Component {
   constructor(props) {
     super(props)
