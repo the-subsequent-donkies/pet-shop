a4360476ada9015e17cdd7cdd3ddc7636c87843b
Eric Guo <ericxguo@gmail.com>
2018-06-05 20:46:13 -0500

    added product-list.js and individual-product.js components

--- client/components/individual-product.js
+++ client/components/individual-product.js
@@ -5,9 +5,11 @@ import React from 'react'
 const IndividualProduct = (props) => {
   const { product } = props
   return (
-    <div>
-      <img src={product.imgUrl} alt={product.name} width='400px' />
-      <div>
+    <div id='product-badge' style={{ width: '25%' }}>
+      <div id='badge-img' style={{ width: '25%'}}>
+        <img id='badge-img' src={product.imgUrl} alt={product.name} />
+      </div>
+      <div className='card-body'>
         <h1>{product.name}</h1>
         <p><strong>Price:</strong> {product.price} <strong>Inventory:</strong> {product.inventory}</p>
       </div>
