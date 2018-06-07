40614f26c9106c6ffe8ec302c930281913e8f1ee
Cody Heslep <codyheslep@gmail.com>
2018-06-07 17:03:07 -0500

    finished api routes for orders and line-items

--- server/api/orders.js
+++ server/api/orders.js
@@ -26,3 +26,23 @@ router.get('/:orderId', async (req, res, next) => {
     next(err)
   }
 })
+
+//POST routes
+router.post('/', async (req, res, next) => {
+  try {
+    const addedOrder = await Order.create(req.body)
+    res.status(201).json(addedOrder)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:orderId', async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.orderId)
+    const addedOrder = await order.update(req.body)
+    res.status(200).json(addedReview)
+  } catch (err) {
+    next(err)
+  }
+})
