3a72957bb65386a8c4e0c8240671c6050f898c49
Eric Guo <ericxguo@gmail.com>
2018-06-11 16:49:23 -0500

    got all admin routes for user control created

--- server/api/users.js
+++ server/api/users.js
@@ -7,8 +7,29 @@ module.exports = router
 
 router.get('/', checkAccess, (req, res, next) => {
   User.findAll({
-    attributes: ['id', 'email']
+    attributes: ['id', 'email', 'isAdmin']
   })
     .then(users => res.json(users))
+    .catch(next)
+})
+
+router.post('/', checkAccess, (req, res, next) => {
+  const { name, description, isAdmin } = req.body
+  User.create({ name, description, isAdmin })
+    .then(user => res.json(user))
+    .catch(next)
+})
+
+router.put('/:userId', checkAccess, (req, res, next) => {
+  const { name, description, isAdmin } = req.body
+  User.findById(req.params.userId)
+    .then(user => user.update({ name, description, isAdmin }))
+    .catch(next)
+})
+
+router.delete('/:userId', checkAccess, (req, res, next) => {
+  User.findById(req.params.userId)
+    .then(user => user.destroy())
+    .then(response => res.json(response))
     .catch(next)
 })
