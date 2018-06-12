a6b068aeaf2d2b7d6be925a1efd2336dbdf69e08
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-06-11 21:28:59 -0500

    hygene

--- server/api/index.js
+++ server/api/index.js
@@ -7,12 +7,9 @@ router.use('/categories', require('./categories'))
 router.use('/reviews', require('./reviews'))
 router.use('/orders', require('./orders'))
 router.use('/lineitems', require('./line-items'))
-<<<<<<< HEAD
 router.use('/search', require('./search-bar'))
-=======
 router.use('/users', require('./users'))
 
->>>>>>> ed659ddc9b2c02a53c2ddb6b171793c12678bd92
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
